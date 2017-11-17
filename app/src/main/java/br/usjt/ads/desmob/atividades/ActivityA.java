83ee2be86e424fcaccfa601cf613ca21daf9eb24
arqdsis <arqdsis@mocli10216.animaedu.intranet>
2017-11-17 21:57:03 -0200

    Initial commit

---
+++ app/src/main/java/br/usjt/ads/desmob/atividades/ActivityA.java
@@ -0,0 +1,15 @@
+package br.usjt.ads.desmob.atividades;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+// create by Thamires
+
+public class ActivityA extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+    }
+}
